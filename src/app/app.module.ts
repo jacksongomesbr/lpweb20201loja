e5980a289cd286c4d7e8ac4039796cbbed2f515f
Jackson Gomes <jgomes@ceulp.edu.br>
2020-04-03 23:47:43 -0300

    configura o app para usar currency default e atualiza o template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID } from '@angular/core';
+import { LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,9 +18,8 @@ registerLocaleData(localePt, 'pt', localePtExtra);
     HttpClientModule
   ],
   providers: [
-    {
-      provide: LOCALE_ID, useValue: 'pt'
-    }
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' }
   ],
   bootstrap: [AppComponent]
 })
